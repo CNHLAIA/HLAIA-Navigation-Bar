94e1fd9c98a1152bfc4538bbe1cceb5a1f8f96c2
HLAIA <813825686@qq.com>
2026-04-24 19:06:46 +0800

    chore(task): archive 00-bootstrap-guidelines

---
+++ src/main/java/com/hlaia/HlaiaNavigationBarApplication.java
@@ -0,0 +1,13 @@
+package com.hlaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlaiaNavigationBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlaiaNavigationBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlaia/HlaiaNavigationBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlaiaNavigationBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
